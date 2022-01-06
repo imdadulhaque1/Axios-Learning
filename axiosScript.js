87ede433e4d1da6be4af325f6a9c991cf6b7d7b6
imdadulhaque1 <imdadulhaqueimdad820@gmail.com>
2022-01-06 17:14:13 +0600

    Show Data using Promises on Browser!

--- axiosScript.js
+++ axiosScript.js
@@ -1,31 +1,42 @@
-document.getElementById('btn').addEventListener('click', makerequest);
+// document.getElementById('btn').addEventListener('click', makerequest);
+document.getElementById("btn").addEventListener('click', dataShowBrowser);
 
-function makerequest(){
-    // alert("Button Clicked!")
-    // config = {
-    //     method: 'get',
-    //     url: 'data.txt'
-    // }
-    // const promiseObj = axios(config);
-    // promiseObj.then((res) =>{
-    //     // console.log(res)
-    //     console.log(`Data: ${res.data}  Status: ${res.status}`);
-    // })
+// function makerequest(){
+//     // alert("Button Clicked!")
+//     // config = {
+//     //     method: 'get',
+//     //     url: 'data.txt'
+//     // }
+//     // const promiseObj = axios(config);
+//     // promiseObj.then((res) =>{
+//     //     // console.log(res)
+//     //     console.log(`Data: ${res.data}  Status: ${res.status}`);
+//     // })
 
-    // axios(config)
-    //     .then((res) =>{
-    //         console.log(`Data: ${res.data}  Status: ${res.status}`);
-    // })
+//     // axios(config)
+//     //     .then((res) =>{
+//     //         console.log(`Data: ${res.data}  Status: ${res.status}`);
+//     // })
 
-    // Get data via axios using get method
+//     // Get data via axios using get method
+//     axios.get('data.txt').then((res) =>{
+//         console.log(res);
+//         console.log(res.data);
+//     }).catch((err) =>{
+//         console.log(err)
+//     })
+//     .then(() =>{
+//         console.log("Error Cleaned!")
+//     })
+// }
+
+
+// ---------> Promise Then | Showing Data in Browser
+function dataShowBrowser(){
+    // console.log("Button Clicked!");
+    //------> Show data using Promises on Browser
     axios.get('data.txt').then((res) =>{
-        console.log(res);
-        console.log(res.data);
-    }).catch((err) =>{
-        console.log(err)
-    })
-    .then(() =>{
-        console.log("Error Cleaned!")
+        // console.log(res.data)
+        document.getElementById("divData").innerText = res.data;
     })
 }
-
