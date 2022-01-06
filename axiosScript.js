febbb3cfd24be271c7a2fd21104c8de7c46affe8
imdadulhaque1 <imdadulhaqueimdad820@gmail.com>
2022-01-06 15:49:28 +0600

    Read data using get methods from data.txt file!

--- axiosScript.js
+++ axiosScript.js
@@ -2,14 +2,25 @@ document.getElementById('btn').addEventListener('click', makerequest);
 
 function makerequest(){
     // alert("Button Clicked!")
-    config = {
-        method: 'get',
-        url: 'data.txt'
-    }
-    const promiseObj = axios(config);
-    promiseObj.then((res) =>{
-        // console.log(res)
-        console.log(`Data: ${res.data}  Status: ${res.status}`);
+    // config = {
+    //     method: 'get',
+    //     url: 'data.txt'
+    // }
+    // const promiseObj = axios(config);
+    // promiseObj.then((res) =>{
+    //     // console.log(res)
+    //     console.log(`Data: ${res.data}  Status: ${res.status}`);
+    // })
+
+    // axios(config)
+    //     .then((res) =>{
+    //         console.log(`Data: ${res.data}  Status: ${res.status}`);
+    // })
+
+    // Get data via axios using get method
+    axios.get('data.txt').then((res) =>{
+        console.log(res);
+        console.log(res.data);
     })
 }
 
