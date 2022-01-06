3af0dcc1656bc42dba42a2378be49c06eeef2ba5
imdadulhaque1 <imdadulhaqueimdad820@gmail.com>
2022-01-06 15:02:54 +0600

    Read data from data.txt file!

--- axiosScript.js
+++ axiosScript.js
@@ -1,6 +1,15 @@
 document.getElementById('btn').addEventListener('click', makerequest);
 
 function makerequest(){
-    alert("Button Clicked!")
+    // alert("Button Clicked!")
+    config = {
+        method: 'get',
+        url: 'data.txt'
+    }
+    const promiseObj = axios(config);
+    promiseObj.then((res) =>{
+        // console.log(res)
+        console.log(`Data: ${res.data}  Status: ${res.status}`);
+    })
 }
 
