87222270baab5c299b5e158c709d6027d507e3dc
imdadulhaque1 <imdadulhaqueimdad820@gmail.com>
2022-01-08 16:51:33 +0600

    customized folder structure for fetching data from remote API

--- fetchDataFrom_JSONFile/axiosScript.js
+++ fetchDataFrom_JSONFile/axiosScript.js
@@ -2,15 +2,15 @@
 document.getElementById("btn").addEventListener('click', dataShowBrowser);
 
 // ---------> Promise | Showing Data in Browser
-function dataShowBrowser(){
-    axios.get('data.json').then((res) =>{
-        // console.log(res.data)
-        document.getElementById("uName").innerText = res.data.name;
-        document.getElementById("uUni").innerText = res.data.uni;
-        document.getElementById("uSub").innerText = res.data.sub;
-        document.getElementById("uDesignation").innerText = res.data.designation;
-    })
-}
+// function dataShowBrowser(){
+//     axios.get('data.json').then((res) =>{
+//         // console.log(res.data)
+//         document.getElementById("uName").innerText = res.data.name;
+//         document.getElementById("uUni").innerText = res.data.uni;
+//         document.getElementById("uSub").innerText = res.data.sub;
+//         document.getElementById("uDesignation").innerText = res.data.designation;
+//     })
+// }
 
 
 // ---------> Async and Await | Showing Data in Browser
@@ -31,23 +31,23 @@ function dataShowBrowser(){
 
 
 // ---------> Async and Await | Try and Catch for error handling | Showing Data in Browser
-// async function dataShowBrowser(){
-//     try{
-//         // alert("Clicked the Button!");
-//         const res = await axios.get('data.json');
-//         // console.log(res.data.name);
+async function dataShowBrowser(){
+    try{
+        // alert("Clicked the Button!");
+        const res = await axios.get('data.json');
+        // console.log(res.data.name);
         
-//         document.getElementById("uName").innerText = res.data.name;
-//         document.getElementById("uUni").innerText = res.data.uni;
-//         document.getElementById("uSub").innerText = res.data.sub;
-//         document.getElementById("uDesignation").innerText = res.data.designation;
+        document.getElementById("uName").innerText = res.data.name;
+        document.getElementById("uUni").innerText = res.data.uni;
+        document.getElementById("uSub").innerText = res.data.sub;
+        document.getElementById("uDesignation").innerText = res.data.designation;
         
-//     }
-//     catch(err){
-//         // console.log(err)
-//         document.getElementById("uName").innerText = err;
-//         document.getElementById("uUni").innerText = err;
-//         document.getElementById("uSub").innerText = err;
-//         document.getElementById("uDesignation").innerText = err;
-//     }
-// }
+    }
+    catch(err){
+        // console.log(err)
+        document.getElementById("uName").innerText = err;
+        document.getElementById("uUni").innerText = err;
+        document.getElementById("uSub").innerText = err;
+        document.getElementById("uDesignation").innerText = err;
+    }
+}
---
+++ fetchDataFrom_RemoteAPI/axiosScript.js
@@ -0,0 +1,2 @@
+
+document.getElementById("btn").addEventListener('click', dataShowBrowser);
