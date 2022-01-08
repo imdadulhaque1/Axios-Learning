7c15e337e73d0c7f61d617ad6ac5f02f10a59f4c
imdadulhaque1 <imdadulhaqueimdad820@gmail.com>
2022-01-08 18:07:52 +0600

    Fetching data from remote API for single data with jsonplaceholder

--- fetchDataFrom_RemoteAPI/axiosScript.js
+++ fetchDataFrom_RemoteAPI/axiosScript.js
@@ -1,2 +1,19 @@
 
 document.getElementById("btn").addEventListener('click', dataShowBrowser);
+
+
+//---------> Promises Then | Single Data
+function dataShowBrowser(){
+    console.log("Button Clicked!")
+    axios('https://jsonplaceholder.typicode.com/posts/1')
+    .then((res) =>{
+        console.log(res.data)
+        document.getElementById('title').innerText = res.data.title
+        document.getElementById('id').innerText = res.data.id
+        document.getElementById('body').innerText = res.data.body
+        // document.get('title').innerText = res.data.title
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+}
