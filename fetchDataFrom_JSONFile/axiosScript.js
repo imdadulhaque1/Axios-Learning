260fc85d77d75cfa58ab1dfc5c5bdb06c988dbf2
imdadulhaque1 <imdadulhaqueimdad820@gmail.com>
2022-01-08 16:15:05 +0600

    data fetch from json using Async and Await | Showing Data in Browser

--- fetchDataFrom_JSONFile/axiosScript.js
+++ fetchDataFrom_JSONFile/axiosScript.js
@@ -11,37 +11,40 @@ document.getElementById("btn").addEventListener('click', dataShowBrowser);
 
 
 // ---------> Async and Await | Showing Data in Browser
-// async function dataShowBrowser(){
-//     console.log("Clicked on Button")
-//     config = {
-//         method: 'get',
-//         url: 'data.txt'
-//     }
-//     const res = await axios(config)
-//     // console.log(res)
-//     // console.log(res.data)
-//     document.getElementById("divData").innerText = res.data;
-// }
+async function dataShowBrowser(){
+    console.log("Clicked on Button")
+    config = {
+        method: 'get',
+        url: 'data.json'
+    }
+    const res = await axios(config)
+    // console.log(res)
+    // console.log(res.data)
+    document.getElementById("uName").innerText = res.data.name;
+    document.getElementById("uUni").innerText = res.data.uni;
+    document.getElementById("uSub").innerText = res.data.sub;
+    // document.getElementById("uDesignation").innerText = res.data.designation;
+}
 
 
 // ---------> Async and Await | Try and Catch for error handling | Showing Data in Browser
-async function dataShowBrowser(){
-    try{
-        // alert("Clicked the Button!");
-        const res = await axios.get('data.json');
-        // console.log(res.data.name);
+// async function dataShowBrowser(){
+//     try{
+//         // alert("Clicked the Button!");
+//         const res = await axios.get('data.json');
+//         // console.log(res.data.name);
         
-        document.getElementById("uName").innerText = res.data.name;
-        document.getElementById("uUni").innerText = res.data.uni;
-        document.getElementById("uSub").innerText = res.data.sub;
-        document.getElementById("uDesignation").innerText = res.data.designation;
+//         document.getElementById("uName").innerText = res.data.name;
+//         document.getElementById("uUni").innerText = res.data.uni;
+//         document.getElementById("uSub").innerText = res.data.sub;
+//         document.getElementById("uDesignation").innerText = res.data.designation;
         
-    }
-    catch(err){
-        // console.log(err)
-        document.getElementById("uName").innerText = err;
-        document.getElementById("uUni").innerText = err;
-        document.getElementById("uSub").innerText = err;
-        document.getElementById("uDesignation").innerText = err;
-    }
-}
+//     }
+//     catch(err){
+//         // console.log(err)
+//         document.getElementById("uName").innerText = err;
+//         document.getElementById("uUni").innerText = err;
+//         document.getElementById("uSub").innerText = err;
+//         document.getElementById("uDesignation").innerText = err;
+//     }
+// }
