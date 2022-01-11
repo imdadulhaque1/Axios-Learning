a00cab6dc093b02fd998ebd9202c5948e8d3684f
imdadulhaque1 <imdadulhaqueimdad820@gmail.com>
2022-01-11 12:54:33 +0600

    SIngle data showing using fake json data

--- fetchDataFrom_RemoteAPI/axiosScript.js
+++ fetchDataFrom_RemoteAPI/axiosScript.js
@@ -4,14 +4,22 @@ document.getElementById("btn").addEventListener('click', dataShowBrowser);
 
 //---------> Promises Then | Single Data
 function dataShowBrowser(){
+    config = {
+        params: {id:1}
+    }
     console.log("Button Clicked!")
-    axios('https://jsonplaceholder.typicode.com/posts/1')
+    axios('https://jsonplaceholder.typicode.com/posts/',config)
     .then((res) =>{
-        console.log(res.data)
-        document.getElementById('title').innerText = res.data.title
-        document.getElementById('id').innerText = res.data.id
-        document.getElementById('body').innerText = res.data.body
-        // document.get('title').innerText = res.data.title
+        // console.log(res.data[0].title)
+        // document.getElementById('title').innerText = res.data[0]
+        // document.getElementById('title').innerText = res.data[0].title
+        // document.getElementById('id').innerText = res.data[0].id
+        // document.getElementById('body').innerText = res.data[0].body
+
+        data = res.data[0]
+        document.getElementById('title').innerText = data.title
+        document.getElementById('id').innerText = data.id
+        document.getElementById('body').innerText = data.body
     })
     .catch((err) => {
         console.log(err);
