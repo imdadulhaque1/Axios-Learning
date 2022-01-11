a2162cd71d8f7db3f7752010c90c18252c9a8307
imdadulhaque1 <imdadulhaqueimdad820@gmail.com>
2022-01-11 16:59:29 +0600

    Single data show on browser and using Async and Await

--- fetchDataFrom_RemoteAPI/axiosScript.js
+++ fetchDataFrom_RemoteAPI/axiosScript.js
@@ -28,32 +28,42 @@ document.getElementById("btn").addEventListener('click', dataShowBrowser);
 
 
 //---------> Promises Then | Multiple Data
-function dataShowBrowser(){
-    config = {
-        params: {id:1}
-    }
-    console.log("Button Clicked!")
-    axios('https://jsonplaceholder.typicode.com/posts/')
-    .then((res) =>{
-        let output = document.getElementById("allPost")
-        console.log(res.data)
-        res.data.forEach(element => {
-            console.log("ID: ", element.id);
-            output.innerHTML+=`
-                <div class="bg-dark m-2 p-3 text-white">
-                    <p>ID: ${element.id}</p>
-                    <p>Title: ${element.title}</p>
-                    <p>Body: ${element.body}</p>
-                </div>
-            `
-
-            // document.getElementById('id').innerText = element.id;
-            // document.getElementById('title').innerText = element.title;
-            // document.getElementById('body').innerText = element.body;
+// function dataShowBrowser(){
+    
+//     axios('https://jsonplaceholder.typicode.com/posts/')
+//     .then((res) =>{
+//         let output = document.getElementById("allPost")
+//         console.log(res.data)
+//         res.data.forEach(element => {
+//             console.log("ID: ", element.id);
+//             output.innerHTML+=`
+//                 <div class="bg-dark m-2 p-3 text-white">
+//                     <p>ID: ${element.id}</p>
+//                     <p>Title: ${element.title}</p>
+//                     <p>Body: ${element.body}</p>
+//                 </div>
+//             `
             
-        });
-    })
-    .catch((err) => {
-        console.log(err);
-    })
+//         });
+//     })
+//     .catch((err) => {
+//         console.log(err);
+//     })
+// }
+
+
+//---------> Async and Await | Single Data
+async function dataShowBrowser(){
+
+    try{
+        console.log("Button Clicked!");
+        const res = await axios("https://jsonplaceholder.typicode.com/posts/1")
+        // console.log(res.data)
+        document.getElementById("id").innerText = res.data.id
+        document.getElementById("title").innerText = res.data.title
+        document.getElementById("body").innerText = res.data.body
+    }
+    catch(err){
+        console.log(err)
+    }
 }
